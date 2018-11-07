2f09d6f7167b6c65b5bd9bc3b699b33b387b2f96
1802448 <1802448@POR7400329.exchange.ad.afpanet>
2018-11-07 16:15:47 +0100

    Ajout dans le main

--- TheGit/src/main/Main.java
+++ TheGit/src/main/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		Stuff stuff = new Stuff();
 		System.out.println(stuff.getHello());
+		System.out.println(stuff.getBonjour());
 	}
 
 }
